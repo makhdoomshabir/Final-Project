aa60928bcbf078415e460e0e6166f280fe8e5129
Daood Khan <dkhan@qa.com>
2020-10-21 10:52:36 +0100

    [PT-47] Updated Ticket Functionality (js)
    Fixed file structure for the project, run npm start from Final-Project/src/main/resources to see results on website
    
    Signed-off-by: Daood Khan <DKhan@qa.com>

--- src/main/resources/src/App.js
+++ src/main/resources/src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import './App.css';
+import './css/App.css';
 import Header from './Components/Header';
 import {Navigation} from './Components/nav';
-// import OpenTickets from '../../src/main/resources/static/js/tickets/OpenTicket';
-// import ResolvedTickets from '../../src/main/resources/static/js/tickets/ResolvedTickets';
-
+import OpenTickets from './Components/tickets/OpenTicket';
+import ResolvedTickets from './Components/tickets/ResolvedTickets';
 
 function App() {
     return (
         <div className="App">
             <Navigation/>
             <Header/>
-            {/*<OpenTickets/>*/}
-            {/*<ResolvedTickets/>*/}
+            <OpenTickets/>
+            <ResolvedTickets/>
         </div>
     );
 }
