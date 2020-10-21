d4edba1f41111fd6158d69eabe83e8cc6b0936d5
Adama <akabba@qa.com>
2020-10-21 15:30:47 +0100

    potential ticket layout

--- frontend/src/Components/OpenTicket.js
+++ frontend/src/Components/OpenTicket.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Card, Button } from "react-bootstrap";
 
 const OpenTickets = () => {
     return(
         <div>
-            <h2> Open Tickets </h2>
-            <body></body>
+            <Card style={{ width: '25rem' }}>
+            <Card.Body>
+                <Card.Title>Issue Title</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Author</Card.Subtitle>
+                <Card.Text>
+                Preview of Description
+
+
+                </Card.Text>
+                <Button variant="primary">Update</Button>
+                <Button variant="primary">Add Solution</Button>
+                <Button variant="primary">Delete</Button>
+            </Card.Body>
+            </Card>
         </div>
     );
 };
