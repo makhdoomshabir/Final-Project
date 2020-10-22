fc1a52dde85a91c9c520133f3035d7c824ac6f54
Krystal <krystalsimm01@me.com>
2020-10-22 15:37:27 +0100

    gave names to EIPs

--- terraform/elastic-ips.tf
+++ terraform/elastic-ips.tf
@@ -1,16 +1,24 @@
 # Elastic IP for NAT Gateway
 resource "aws_eip" "nat_gw_eip" {
   vpc = true
+
+  tags = {
+    Name = "NAT GW IP"
+  }
 }
 
-# # Elastic IP for bastion server
-# resource "aws_eip" "bastion_eip" {
-#   instance = aws_instance.Bastion.id
-#   vpc      = true
-# }
+# Elastic IP for bastion server
+resource "aws_eip" "bastion_eip" {
+  instance = aws_instance.Bastion.id
+  vpc      = true
 
-# # EIP association for bastion server
-# resource "aws_eip_association" "eip_assoc" {
-#   instance_id   = aws_instance.Bastion.id
-#   allocation_id = aws_eip.bastion_eip.id
-# }
+  tags = {
+    Name = "Bastion Instance IP"
+  }
+}
+
+# EIP association for bastion server
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.Bastion.id
+  allocation_id = aws_eip.bastion_eip.id
+}
