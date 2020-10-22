e0867f31cafcec56919159f4519099c9ae8a0dbb
Krystal <krystalsimm01@me.com>
2020-10-22 14:44:00 +0100

    edited security-groups

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -165,9 +165,9 @@ resource "aws_security_group" "bastion-sg" {
   # HTTP Traffic
   ingress {
     description = "HTTP"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
