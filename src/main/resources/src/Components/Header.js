fa1f395998389940594ab746c36b6fef1d9d7b46
Adama <akabba@qa.com>
2020-10-22 16:15:15 +0100

    added jumbotron feature to header

--- src/main/resources/src/Components/Header.js
+++ src/main/resources/src/Components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {Jumbotron} from "react-bootstrap";
 
 const Header = () => {
   return (
     <div>
-        <h1>Trainee Ticket System</h1>
-        <h3>Got a Problem? Leave a Ticket.</h3>
+        <Jumbotron id="homepageHeader">
+            <h1>Got a Problem? Leave a Ticket</h1>
+            <p>
+                Perhaps you're finding certain concepts in the academy difficult to understand or maybe you're you've
+                a roadblock in your project and you need help from a trainer. Leave a ticket with your issue described
+                and someone will get back to you.
+
+                Alternatively look through the resolved tickets and their solution to see if your issue has already been
+                resolved.
+            </p>
+        </Jumbotron>
     </div>
   );
 };
