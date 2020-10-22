f88bd900525fed48a864ace83db5d1ed77a3e5e5
Krystal <krystalsimm01@me.com>
2020-10-22 14:40:31 +0100

    removed elastic ip resource and added to elastic-ips.tf

--- terraform/nat-gateway.tf
+++ terraform/nat-gateway.tf
@@ -3,8 +3,3 @@ resource "aws_nat_gateway" "nat-gw" {
   allocation_id = aws_eip.nat_gw_eip.id
   subnet_id     = aws_subnet.subnet-4-prod.id
 }
-
-# Elastic IP for NAT Gateway
-resource "aws_eip" "nat_gw_eip" {
-  vpc = true
-}
