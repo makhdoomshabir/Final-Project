266e996733697f1960c9ca9b37616766f9504d2e
Adama <akabba@qa.com>
2020-10-23 15:20:52 +0100

    spelling errors fixed

--- src/main/resources/src/Components/Header.js
+++ src/main/resources/src/Components/Header.js
@@ -8,13 +8,13 @@ const Header = () => {
             <h1>Got a Problem? Leave a Ticket.</h1>
             <blockquote className="bg-dark text-white">
                 <p>
-                    Perhaps you're finding certain concepts in the academy difficult to understand or maybe you're you've
+                    Perhaps you're finding certain concepts in the academy difficult to understand or maybe you've hit
                     a roadblock in your project and you need help from a trainer. Leave a ticket with your issue described
                     and someone will get back to you.
                 </p>
 
                 <p>
-                    Alternatively look through the resolved tickets and their solution to see if your issue has already been
+                    Alternatively look through the resolved tickets and their solutions to see if your issue has already been
                     resolved.
                 </p>
                 <footer className="blockquote-footer">
