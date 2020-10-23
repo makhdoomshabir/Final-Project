88aba80d2ee8f9af312d24f717f340c715eec5c2
Adama <akabba@qa.com>
2020-10-23 15:36:02 +0100

    Added jumbtron to top of page

--- src/main/resources/src/Components/Pages/HomeSoftware.js
+++ src/main/resources/src/Components/Pages/HomeSoftware.js
@@ -1,9 +1,15 @@
 import React from "react";
+import {Jumbotron, Button} from "react-bootstrap";
 
 const HomeSoftware = () =>  {
     return(
         <div>
-
+            <Jumbotron className="bg-dark text-white" id="HomeSoftwareHeader">
+                <h1>Software Development</h1>
+                <p>
+                    Use the Cohort dropdown to filter for tickets specific to your cohort.
+                </p>
+            </Jumbotron>
         </div>
     )
 
