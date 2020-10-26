781c89f53b4d401d4fea03ca37e9bc909c8aa1a9
Adama <akabba@qa.com>
2020-10-26 22:38:18 +0000

    added a tags so that links on welcome page take to appropriate page

--- src/main/resources/src/Components/WelcomeOptions.js
+++ src/main/resources/src/Components/WelcomeOptions.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {Button, Card, CardDeck, NavDropdown} from "react-bootstrap";
 import cloud from './images/cloud.png';
+import {faPlusSquare, faArrowRight} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const WelcomeOptions = () => {
     return (
@@ -11,21 +13,27 @@ const WelcomeOptions = () => {
                 <Card.Body>
                     <Card.Title> <h3>Software Development</h3> </Card.Title>
                 </Card.Body>
-                <Button > See Tickets</Button>
+                <a href="/software-development">
+                <Button > <FontAwesomeIcon icon={faArrowRight} /></Button>
+                </a>
             </Card>
 
             <Card >
                 <Card.Body>
                     <Card.Title> <h3>Cloud Computing</h3> </Card.Title>
                 </Card.Body>
+                <a href="/cloud-computing">
                 <Button> See Tickets</Button>
+                </a>
             </Card>
 
             <Card >
                 <Card.Body>
                     <Card.Title><h3>Devops</h3></Card.Title>
                 </Card.Body>
+                <a href="/dev-ops">
                 <Button> See Tickets</Button>
+                </a>
             </Card>
         </CardDeck>
             <CardDeck id="WelcomeOptions"  >
@@ -33,14 +41,18 @@ const WelcomeOptions = () => {
                 <Card.Body>
                     <Card.Title><h3>Robotic Process Automation</h3></Card.Title>
                 </Card.Body>
+                <a href="/robotic-process-automation">
                 <Button> See Tickets</Button>
+                </a>
             </Card>
 
             <Card >
                 <Card.Body>
                     <Card.Title><h3>Pega</h3></Card.Title>
                 </Card.Body>
-                <Button> See Tickets</Button>
+                <a>
+                <Button href="/pega"> See Tickets</Button>
+                </a>
             </Card>
         </CardDeck>
     </div>
