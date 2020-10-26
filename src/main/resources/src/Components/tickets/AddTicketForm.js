d0e62e1d0af297910c7f358f7adf27ae7aeecd52
Adama <akabba@qa.com>
2020-10-26 21:17:11 +0000

    added the code to POST request with axios and send data to be viewed on other pages.

--- src/main/resources/src/Components/tickets/AddTicketForm.js
+++ src/main/resources/src/Components/tickets/AddTicketForm.js
@@ -1,56 +1,136 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Form, Col, Button} from 'react-bootstrap';
-
-const AddTicketForm = () => {
-    return(
-        <div>
-            <Form id="ticketForm">
-            <Form.Row>
-                <Form.Group as={Col} id="cohort">
-                <Form.Label>Cohort</Form.Label>
-                <Form.Control as="select" defaultValue="Choose...">
-                    <option>Choose...</option>
-                    <option>Software Development</option>
-                    <option>Cloud Computing</option>
-                    <option>DevOps</option>
-                    <option>Robotic Process Automation</option>
-                    <option>Pega</option>
-                </Form.Control>
-                </Form.Group>
-
-                <Form.Group as={Col} id="author">
-                <Form.Label>Author</Form.Label>
-                <Form.Control type="text"/>
-                </Form.Group>
-            </Form.Row>
-
-            <Form.Group id="issueTitle">
-                <Form.Label>Issue Title</Form.Label>
-                <Form.Control type="text"/>
-            </Form.Group>
-
-            <Form.Group id="issueDescription">
-                <Form.Label>Issue Description</Form.Label>
-                <Form.Control as="textarea" rows="4"/>
-            </Form.Group>
-
-            <Form.Group id="links">
-                <Form.Label>Links</Form.Label>
-                <Form.Control type="text"/>
-                <button> Add Link</button>
-            </Form.Group>
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faPlusSquare, faSave, faUndo} from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
 
-            <Form.Group id="formGridCheckbox">
-                <Form.Check type="checkbox" label="I understand that my Issue will be posted to the public ticket board" />
-            </Form.Group>
+export default class extends Component{
+    constructor(props) {
+        super(props);
+        this.state= this.initialState;
+        this.ticketChange = this.ticketChange.bind(this);
+        this.submitTicket = this.submitTicket.bind(this);
+    }
 
-            <Button variant="primary" type="submit">
-                Add Issue
-            </Button>
-            </Form>
-        </div>
-    );
+    initialState = {
+        cohort:'', author:'', title:'', description:'', links:''
+    }
+
+    resetTicket = () => {
+        this.setState(() => this.initialState);
+    }
+
+    submitTicket(event) {
+        event.preventDefault();
+
+        const tickets = {
+                cohort: this.state.cohort,
+                author: this.state.author,
+                title: this.state.title,
+                description: this.state.description,
+                links: this.state.links
+        };
+
+        axios.post("http://localhost:8080/createTicket", tickets)
+            .then(response => {
+                if(response.data != null) {
+                    this.setState(this.initialState);
+                    alert("Ticket Saved Successfully")
+                }
+            })
+    }
+
+    ticketChange(event){
+        this.setState({[event.target.name]:event.target.value})
+    }
+
+    render() {
+        const {cohort, author, title, description, links} = this.state;
+
+        return (
+            <div>
+                <Form onReset={this.resetTicket} onSubmit={this.submitTicket} id="ticketForm">
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="formCohort" >
+                            <Form.Label>Cohort</Form.Label>
+                            <Form.Control
+                                required autoComplete="off"
+                                as="select"
+                                defaultValue="Choose..."
+                                name="cohort"
+                                value={cohort}
+                                onChange={this.ticketChange}>
+                                <option>Choose...</option>
+                                <option>Software Development</option>
+                                <option>Cloud Computing</option>
+                                <option>DevOps</option>
+                                <option>Robotic Process Automation</option>
+                                <option>Pega</option>
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formAuthor">
+                            <Form.Label>Author</Form.Label>
+                            <Form.Control
+                                required autoComplete="off"
+                                type="text"
+                                name="author"
+                                value={author}
+                                onChange={this.ticketChange}
+                            />
+                        </Form.Group>
+                    </Form.Row>
+
+                    <Form.Group controlId="formTitle">
+                        <Form.Label>Issue Title</Form.Label>
+                        <Form.Control
+                            required autoComplete="off"
+                            type="text"
+                            name="title"
+                            value={title}
+                            onChange={this.ticketChange}/>
+                    </Form.Group>
+
+                    <Form.Group controlId="formDescription">
+                        <Form.Label>Issue Description</Form.Label>
+                        <Form.Control
+                            required autoComplete="off"
+                            as="textarea"
+                            rows="4"
+                            name="description"
+                            value={description}
+                            onChange={this.ticketChange}/>
+                    </Form.Group>
+
+                    <Form.Group controlId="formLinks">
+                        <Form.Label>Links</Form.Label>
+                        <Form.Control
+                            autoComplete="off"
+                            type="text"
+                            name="links"
+                            value={links}
+                            onChange={this.ticketChange}/>
+                        <button> Add Link</button>
+                    </Form.Group>
+
+
+                    <Form.Group ControlID="formGridCheckbox">
+                        <Form.Check type="checkbox"
+                                    label="I understand that my Issue will be posted to the public ticket board"/>
+                    </Form.Group>
+
+                    <Button variant="success" type="submit">
+                        <FontAwesomeIcon icon={faPlusSquare} /> Add Ticket
+
+                    </Button>{'  '}
+
+                    <Button variant="info" type="reset">
+                        <FontAwesomeIcon icon={faUndo} /> Reset Form
+                    </Button>
+
+                </Form>
+            </div>
+        );
+    };
 };
-
-export default AddTicketForm;
