53967c2c4500c899624260b9b186c5a2dbbff8fd
Adama <akabba@qa.com>
2020-10-26 22:17:18 +0000

    Added toast for successful Ticket Display

--- src/main/resources/src/Components/tickets/AddTicketForm.js
+++ src/main/resources/src/Components/tickets/AddTicketForm.js
@@ -3,12 +3,14 @@ import { Form, Col, Button} from 'react-bootstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faPlusSquare, faSave, faUndo} from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
+import MyToast from "../MyToast";
 
 
 export default class extends Component{
     constructor(props) {
         super(props);
         this.state= this.initialState;
+        this.state.show= false;
         this.ticketChange = this.ticketChange.bind(this);
         this.submitTicket = this.submitTicket.bind(this);
     }
@@ -35,10 +37,13 @@ export default class extends Component{
         axios.post("http://localhost:8080/createTicket", tickets)
             .then(response => {
                 if(response.data != null) {
-                    this.setState(this.initialState);
-                    alert("Ticket Saved Successfully")
+                    this.setState({"show":true})
+                    setTimeout(() => this.setState({"show":false}), 3000);
+                } else {
+                    this.setState({"show":false})
                 }
-            })
+            });
+        this.setState(this.initialState);
     }
 
     ticketChange(event){
@@ -49,88 +54,95 @@ export default class extends Component{
         const {cohort, author, title, description, links} = this.state;
 
         return (
-            <div>
-                <Form onReset={this.resetTicket} onSubmit={this.submitTicket} id="ticketForm">
-                    <Form.Row>
-                        <Form.Group as={Col} controlId="formCohort" >
-                            <Form.Label>Cohort</Form.Label>
-                            <Form.Control
-                                required autoComplete="off"
-                                as="select"
-                                defaultValue="Choose..."
-                                name="cohort"
-                                value={cohort}
-                                onChange={this.ticketChange}>
-                                <option>Choose...</option>
-                                <option>Software Development</option>
-                                <option>Cloud Computing</option>
-                                <option>DevOps</option>
-                                <option>Robotic Process Automation</option>
-                                <option>Pega</option>
-                            </Form.Control>
-                        </Form.Group>
+            <>
 
-                        <Form.Group as={Col} controlId="formAuthor">
-                            <Form.Label>Author</Form.Label>
+                <div style={{"display":this.state.show ? "block" : "none"}}>
+                 <MyToast children={{show:this.state.show, message:"Book Saved Successfully"}}/>
+                </div>
+
+                <div>
+                    <Form onReset={this.resetTicket} onSubmit={this.submitTicket} id="ticketForm">
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formCohort" >
+                                <Form.Label>Cohort</Form.Label>
+                                <Form.Control
+                                    required autoComplete="off"
+                                    as="select"
+                                    defaultValue="Choose..."
+                                    name="cohort"
+                                    value={cohort}
+                                    onChange={this.ticketChange}>
+                                    <option>Choose...</option>
+                                    <option>Software Development</option>
+                                    <option>Cloud Computing</option>
+                                    <option>DevOps</option>
+                                    <option>Robotic Process Automation</option>
+                                    <option>Pega</option>
+                                </Form.Control>
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formAuthor">
+                                <Form.Label>Author</Form.Label>
+                                <Form.Control
+                                    required autoComplete="off"
+                                    type="text"
+                                    name="author"
+                                    value={author}
+                                    onChange={this.ticketChange}
+                                />
+                            </Form.Group>
+                        </Form.Row>
+
+                        <Form.Group controlId="formTitle">
+                            <Form.Label>Issue Title</Form.Label>
                             <Form.Control
                                 required autoComplete="off"
                                 type="text"
-                                name="author"
-                                value={author}
-                                onChange={this.ticketChange}
-                            />
+                                name="title"
+                                value={title}
+                                onChange={this.ticketChange}/>
                         </Form.Group>
-                    </Form.Row>
 
-                    <Form.Group controlId="formTitle">
-                        <Form.Label>Issue Title</Form.Label>
-                        <Form.Control
-                            required autoComplete="off"
-                            type="text"
-                            name="title"
-                            value={title}
-                            onChange={this.ticketChange}/>
-                    </Form.Group>
+                        <Form.Group controlId="formDescription">
+                            <Form.Label>Issue Description</Form.Label>
+                            <Form.Control
+                                required autoComplete="off"
+                                as="textarea"
+                                rows="4"
+                                name="description"
+                                value={description}
+                                onChange={this.ticketChange}/>
+                        </Form.Group>
 
-                    <Form.Group controlId="formDescription">
-                        <Form.Label>Issue Description</Form.Label>
-                        <Form.Control
-                            required autoComplete="off"
-                            as="textarea"
-                            rows="4"
-                            name="description"
-                            value={description}
-                            onChange={this.ticketChange}/>
-                    </Form.Group>
-
-                    <Form.Group controlId="formLinks">
-                        <Form.Label>Links</Form.Label>
-                        <Form.Control
-                            autoComplete="off"
-                            type="text"
-                            name="links"
-                            value={links}
-                            onChange={this.ticketChange}/>
-                        <button> Add Link</button>
-                    </Form.Group>
+                        <Form.Group controlId="formLinks">
+                            <Form.Label>Links</Form.Label>
+                            <Form.Control
+                                autoComplete="off"
+                                type="text"
+                                name="links"
+                                value={links}
+                                onChange={this.ticketChange}/>
+                            <button> Add Link</button>
+                        </Form.Group>
 
 
-                    <Form.Group ControlID="formGridCheckbox">
-                        <Form.Check type="checkbox"
-                                    label="I understand that my Issue will be posted to the public ticket board"/>
-                    </Form.Group>
+                        <Form.Group ControlID="formGridCheckbox">
+                            <Form.Check type="checkbox"
+                                        label="I understand that my Issue will be posted to the public ticket board"/>
+                        </Form.Group>
 
-                    <Button variant="success" type="submit">
-                        <FontAwesomeIcon icon={faPlusSquare} /> Add Ticket
+                        <Button variant="success" type="submit">
+                            <FontAwesomeIcon icon={faPlusSquare} /> Add Ticket
 
-                    </Button>{'  '}
+                        </Button>{'  '}
 
-                    <Button variant="info" type="reset">
-                        <FontAwesomeIcon icon={faUndo} /> Reset Form
-                    </Button>
+                        <Button variant="info" type="reset">
+                            <FontAwesomeIcon icon={faUndo} /> Reset Form
+                        </Button>
 
-                </Form>
-            </div>
+                    </Form>
+                </div>
+            </>
         );
     };
 };
