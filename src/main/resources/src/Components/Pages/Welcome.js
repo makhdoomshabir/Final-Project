c52342d04a532549dec0c77a78a27c578254a9b2
Adama <akabba@qa.com>
2020-11-04 16:45:13 +0000

    Testing it how it works section for front page

--- src/main/resources/src/Components/Pages/Welcome.js
+++ src/main/resources/src/Components/Pages/Welcome.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Header";
 import WelcomeOptions from "../WelcomeOptions";
+import HowItWorks from "../HowItWorks";
 
 const Welcome = () => {
     return (
         <div>
         <Header/>
+        <HowItWorks/>
         <WelcomeOptions/>
         </div>
     )
