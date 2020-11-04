589925b0d2a3565e24ed7559f55dd67e231ae29c
Krystal <krystalsimm01@me.com>
2020-11-04 08:32:12 +0000

    Edited script

--- ansible/aws-cli.sh
+++ ansible/aws-cli.sh
@@ -3,4 +3,4 @@
 sudo apt update -y && sudo apt install curl unzip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ./aws/install << EOF
+sudo ./aws/install
