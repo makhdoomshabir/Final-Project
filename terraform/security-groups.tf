6cffb0cb29c5a4ca1a1daba9651446bc88c5fffb
Krystal <krystalsimm01@me.com>
2020-11-05 19:02:08 +0000

    Edited bastion security group

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -168,7 +168,7 @@ resource "aws_security_group" "bastion-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["46.64.73.251/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # HTTP Traffic
